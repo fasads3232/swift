bde44b3099aa1fc9a629da87a4e3b850c6d47f74
fasads3232 <maga14881337@icloud.com>
2024-05-20 16:56:24 +0300

    base task done

--- Counter/Counter/ViewController.swift
+++ Counter/Counter/ViewController.swift
@@ -11,9 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        labelWork.text = "Значение счетчика : \(counter)"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var labelWork: UILabel!
+    var counter : Int = 0
+    @IBAction func plusOne(_ sender: UIButton) {
+        counter+=1
+        labelWork.text = "Значение счетчика : \(counter)"
+    }
+    
 }
 
