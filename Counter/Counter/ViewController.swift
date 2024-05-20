cee8b00d12682ef0c02a7bab74943d9979bdb50a
fasads3232 <maga14881337@icloud.com>
2024-05-20 18:55:50 +0300

    сделал задачу со звездочкой

--- Counter/Counter/ViewController.swift
+++ Counter/Counter/ViewController.swift
@@ -5,22 +5,50 @@
 //  Created by Андрей on 20.05.2024.
 //
 
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var results: UITextView!
+    var counter: Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        labelWork.text = "Значение счетчика : \(counter)"
-        // Do any additional setup after loading the view.
+        results.text = "История изменений: \n"
     }
 
-    @IBOutlet weak var labelWork: UILabel!
-    var counter : Int = 0
-    @IBAction func plusOne(_ sender: UIButton) {
-        counter+=1
-        labelWork.text = "Значение счетчика : \(counter)"
+    @IBAction func add(_ sender: Any) {
+        counter += 1
+        let date = Date()
+        let formattedDate = formatDate(date)
+        results.text.append("[\(formattedDate)]: значение изменено на +1 \n")
     }
-    
+
+    @IBAction func degree(_ sender: Any) {
+        counter -= 1
+        if counter <= 0{
+            let date = Date()
+            let formattedDate = formatDate(date)
+            results.text.append("[\(formattedDate)]: попытка уменьшить значение счётчика ниже 0\n")
+            return
+        }
+        let date = Date()
+        let formattedDate = formatDate(date)
+        results.text.append("[\(formattedDate)]: значение изменено на -1 \n")
+    }
+
+    @IBAction func erase(_ sender: Any) {
+        counter = 0
+        let date = Date()
+        let formattedDate = formatDate(date)
+        results.text.append("[\(formattedDate)]: значение сброшено")
+    }
+
+    func formatDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yy HH:mm:ss"
+        formatter.timeZone = TimeZone(identifier: "Europe/Moscow")
+        return formatter.string(from: date)
+    }
 }
-
